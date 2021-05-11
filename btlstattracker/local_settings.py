6adaff1a6c2adde74ed6cfcd2e7c0a38fd06eb59
Jesse Wallace <tonyiommisg@gmail.com>
2021-05-11 16:54:25 -0700

    fixed DATABASE_URL

--- btlstattracker/local_settings.py
+++ btlstattracker/local_settings.py
@@ -5,7 +5,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] deprecated due to heroku not updating their database url and not allowing me to change it either.
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # or other relevant config var
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 # db_connection_string = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 # SQLALCHEMY_DATABASE_URI = db_connection_string
 SQLALCHEMY_TRACK_MODIFICATIONS = False
